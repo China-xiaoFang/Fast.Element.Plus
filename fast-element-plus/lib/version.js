b0509a924e62dfd078bffcf6bbc1072f86d28328
小方 <2875616188@qq.com>
2026-01-20 01:14:43 +0800

    发布 `v1.0.14` 版本。

--- fast-element-plus/lib/version.js
+++ fast-element-plus/lib/version.js
@@ -1,2 +1,2 @@
-"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});exports.version="1.0.13";
+"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});exports.version="1.0.14";
 //# sourceMappingURL=version.js.map
