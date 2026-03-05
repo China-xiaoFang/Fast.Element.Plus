81eee801683d8fee16a32b062ad1eda5f27cbc70
小方 <2875616188@qq.com>
2026-03-06 02:12:13 +0800

    `v1.0.19`

--- fast-element-plus/lib/version.js
+++ fast-element-plus/lib/version.js
@@ -1,2 +1,2 @@
-"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});exports.version="1.0.17";
+"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});exports.version="1.0.19";
 //# sourceMappingURL=version.js.map
